068d33a8f5e166f192603eaa6f941928a95e8838
Sebastian Quevedo <sebastian.quevedo02@gmail.com>
2022-09-06 07:14:51 -0500

    Añadiendo el pdoer listar los productos

--- Routes/product.adm.js
+++ Routes/product.adm.js
@@ -14,4 +14,16 @@ const getProductData = () => {
     return JSON.parse(jsonData)    
 }
 
+
+// Lista todos los productos
+productRoutes.get('/admin/product', (req, res) => {
+    fs.readFile(dataPath, 'utf8', (err, data) => {
+      if (err) {
+        throw err;
+      }
+      res.send(JSON.parse(data));
+    });
+  });
+
+
 module.exports = productRoutes
